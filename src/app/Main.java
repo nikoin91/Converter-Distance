2c2dc299cd1afd4c087cdfe66ceb192df4502623
Nikolaev Igor <nikolaevin91@gmail.com>
2024-10-30 13:40:56 +0200

    Add Kms Converter

--- src/app/Main.java
+++ src/app/Main.java
@@ -6,13 +6,20 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Converter App.");
+        System.out.println("Version 0.1");
 
         float kms = 4;
+        float mi = 10;
         float miles = convKmsToMiles(kms);
-        System.out.printf("Result is %.2f miles", miles);
+        float kilom = convMilesToKms(mi);
+        System.out.printf("Result is %.2f miles" + System.lineSeparator()
+                        + "Result is %.2f kilometers", miles , kilom);
     }
 
     private static float convKmsToMiles(float kms){
         return kms / CONV_K;
+    }
+    private static float convMilesToKms(float mi) {
+        return mi * CONV_K;
     }
 }
