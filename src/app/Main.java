7239c10419760d1c68d4de6629305b812e48526b
Nikolaev Igor <nikolaevin91@gmail.com>
2024-10-30 13:19:49 +0200

    Add Miles Converter

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,7 +2,17 @@ package app;
 
 public class Main {
 
+    private static final float CONV_K = 1.60934F;
+
     public static void main(String[] args) {
         System.out.println("Converter App.");
+
+        float kms = 4;
+        float miles = convKmsToMiles(kms);
+        System.out.printf("Result is %.2f miles", miles);
+    }
+
+    private static float convKmsToMiles(float kms){
+        return kms / CONV_K;
     }
 }
